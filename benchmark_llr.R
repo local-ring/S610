26045d17c436ac4feb659461968127adedfd0610
local-ring <lial0921.miu@gmail.com>
2023-10-26 21:44:31 -0400

    setwd()

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,6 +1,10 @@
-getwd()
-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-# cope from https://stackoverflow.com/questions/35074911/how-to-set-the-current-file-location-as-the-default-working-directory-in-r-progr
+# getwd()
+# setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+# # cope from https://stackoverflow.com/questions/35074911/how-to-set-the-current-file-location-as-the-default-working-directory-in-r-progr
+## the above cannot allow me to run script outside the Rstudio, need to do it 
+## in the shell
+
+setwd("/Users/aolongli/Desktop/2023-Fall/S610/S610")
 source("llr_functions.R")
 library(bench)
 
