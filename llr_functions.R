88dd0a649d7ed15b727c4e9390f8875b6b278d5a
local-ring <lial0921.miu@gmail.com>
2023-10-26 22:05:35 -0400

    speed-up take 2

--- llr_functions.R
+++ llr_functions.R
@@ -3,20 +3,16 @@ make_predictor_matrix = function(x) {
   return(mat)
 }
 
-make_weight_matrix = function(x, z, omega){
-  W_z <- diag(abs(z-x)/omega)
-  return(W_z)
-}
-
 make_weight_vector = function(x, z, omega){
   weights <- abs(z-x)/omega
   return(weights)
 }
 
 compute_f_hat = function(z, x, y, omega) {
-  Wz = make_weight_vector(x, z, omega)
+  Wz = make_weight_vector(z, x, omega)
   X = make_predictor_matrix(x)
-  WX = apply(X, 2, function(col) col * Wz)
+  
+  WX = sweep(X, 1, Wz, `*`)
   Wy = Wz * y
   
   f_hat = c(1, z) %*% solve(t(X) %*% WX) %*% t(X) %*%  Wy
